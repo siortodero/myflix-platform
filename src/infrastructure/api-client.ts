cdfde50443213526cd83729c3388224ee285f5a1
Andrea Ragogna <ragognaa00@gmail.com>
2023-12-27 15:32:53 +0100

    feat: api client

---
+++ src/infrastructure/api-client.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_API_URI}/${process.env.NEXT_PUBLIC_CURRENT_API_VERSION}`,
+  headers: {
+    Accept: "application/json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN_AUTH}`,
+  },
+});
+
+export const getApi = <TResponse = any, TParams = any>(
+  relativeUri: string,
+  params?: TParams
+) =>
+  axiosClient.get<TResponse>(`/${relativeUri}`, {
+    params,
+  });
+
+export const postApi = <TResponse = void>(relativeUri: string, data: any) =>
+  axiosClient.post<TResponse>(`/${relativeUri}`, data);
+
+export const putApi = <TResponse = void>(relativeUri: string, data: any) =>
+  axiosClient.put<TResponse>(`/${relativeUri}`, data);
+
+export const deleteApi = <TResponse = void>(relativeUri: string) =>
+  axiosClient.delete<TResponse>(`/${relativeUri}`);
