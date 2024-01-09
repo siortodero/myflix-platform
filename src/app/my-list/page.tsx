ab7c7553d5433805eb115eded8dcf7f749dd13e2
Andrea Ragogna <ragognaa00@gmail.com>
2024-01-09 11:47:58 +0100

    feat: home page placeholder

--- src/app/my-list/page.tsx
+++ src/app/my-list/page.tsx
@@ -1,3 +1,7 @@
 export default function MyList() {
-  return <div>My list</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-semibold text-white">Coming soon...</h1>
+    </div>
+  );
 }
