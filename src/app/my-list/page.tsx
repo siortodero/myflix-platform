e54372b2f31864983d331a8073a984f871251ea5
Andrea Ragogna <ragognaa00@gmail.com>
2024-01-10 10:31:23 +0100

    feat: placeholder page translation

--- src/app/my-list/page.tsx
+++ src/app/my-list/page.tsx
@@ -1,7 +1,14 @@
+import { Translation } from "@/components";
+
 export default function MyList() {
   return (
-    <div className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-semibold text-white">Coming soon...</h1>
+    <div className="flex min-h-[calc(100vh-64px)] flex-col items-center p-24">
+      <h1 className="mb-8 text-6xl font-semibold text-white">
+        <Translation label="coming-soon.title" />
+      </h1>
+      <p className="text-white">
+        <Translation label="coming-soon.description" />
+      </p>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { Translation } from "@/components";
 import { FC } from "react";
 
 const Home: FC = () => {
   return (
-    <div className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-semibold text-white">Coming soon...</h1>
+    <div className="flex min-h-[calc(100vh-64px)] flex-col items-center p-24">
+      <h1 className="mb-8 text-6xl font-semibold text-white">
+        <Translation label="coming-soon.title" />
+      </h1>
+      <p className="text-white">
+        <Translation label="coming-soon.description" />
+      </p>
     </div>
   );
 };
